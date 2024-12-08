118b512625b281f6ee20cb04bafb32d4829714c5
techmasterlk <155933963+techmasterlk@users.noreply.github.com>
2024-12-08 13:30:57 +0530

    add s3 bucket

--- medusa-config.ts
+++ medusa-config.ts
@@ -48,8 +48,17 @@ module.exports = defineConfig({
       options: {
         providers: [
           {
-            resolve: "@medusajs/medusa/file-local",
-            id: "local",
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+              // other options...
+            },
           },
         ],
       },
